4f3b97da1c18c49b4d2df5409bd6a74d3827e6c9
ingrid917 <ingridmatei17@gmail.com>
2025-10-20 14:00:45 +0300

    1. commit

---
+++ src/Note.java
@@ -0,0 +1,15 @@
+public class Note {
+    private int grade;
+
+    public Note(int grade){
+        this.grade = grade;
+    }
+
+    public int getGrade(){
+        return grade;
+    }
+    public void setGrade(int grade){
+        this.grade = grade;
+    }
+
+}
